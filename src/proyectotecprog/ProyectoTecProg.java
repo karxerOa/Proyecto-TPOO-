c40c5bb2f395c9cc1774a54206518c8b6c10bde7
NilberAvila <N00362400@upn.pe>
2025-04-04 16:30:13 -0500

    Holladsfdf

--- src/proyectotecprog/ProyectoTecProg.java
+++ src/proyectotecprog/ProyectoTecProg.java
@@ -16,6 +16,7 @@ public class ProyectoTecProg {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Si les llega funciono");
+        System.out.println("hiuh");
     }
     
 }
